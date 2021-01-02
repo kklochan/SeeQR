00e959eba8f3ab195c174eef7e9e0731968b88da
Katie Klochan <katieklochan@Katies-MBP.fios-router.home>
2021-01-02 14:45:35 -0500

    one more time trying build script in start

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+// const PG = require('pg');
+// const parse = require('pg-connection-string').parse;
+
+// config = parse('postgres://someuser:somepassword@somehost:381/somedatabase')
+
+// const connection = pg.createConnection
