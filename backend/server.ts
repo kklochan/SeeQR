d34e492eaa2a3a8f3677698696229339406cea6d
Katie Klochan <katieklochan@Katies-MBP.fios-router.home>
2021-01-02 12:08:41 -0500

    put start back to prod env and changed port 3000 to 8081

--- backend/server.ts
+++ backend/server.ts
@@ -8,5 +8,5 @@ server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
-const port = process.env.port || 3000;
-server.listen(port, () => console.log('listening on port 3000'));
+const port = process.env.port || 8081;
+server.listen(port, () => console.log('listening on port 8081'));
